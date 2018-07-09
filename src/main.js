7a39ce2a786251d995e5d075e5023edc12765753
Tom LanFranca <ittechtommy@gmail.com>
2018-07-09 08:35:02 -0500

    Setup and Initialized firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 import { store } from "./store/index"
+import * as firebase from "firebase"
 
 Vue.use(Vuetify)
 
@@ -14,5 +15,14 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyCL-bHW5RavI49dliKEFu8Jqy_XaVJrV2M",
+      authDomain: "dev-meetup-c52a3.firebaseapp.com",
+      databaseURL: "https://dev-meetup-c52a3.firebaseio.com",
+      projectId: "dev-meetup-c52a3",
+      storageBucket: "dev-meetup-c52a3.appspot.com"
+    });
+  }
 })
